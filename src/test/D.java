2931494149ff2093df90415d4411855eda0ece03
Черепанов Сергей (4092324) <slcherepanov@dev.vtb.ru>
2025-09-16 16:57:00 +0300

    Task D. Две фишки

---
+++ src/test/D.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class D {
+
+    // Если ответ существует, верните список из двух элементов
+    // Если нет - то верните пустой список
+    private static List<Integer> twoSum(List<Integer> arr, int targetSum) {
+        var result = new ArrayList<Integer>();
+        var list = arr.stream().distinct().collect(Collectors.toList());
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                var sum = list.get(i) + list.get(j);
+
+                if (sum == targetSum) {
+                    result.add(list.get(i));
+                    result.add(list.get(j));
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = readInt(reader);
+            List<Integer> arr = readList(reader);
+            int targetSum = readInt(reader);
+            List<Integer> result = twoSum(arr, targetSum);
+            if (result.isEmpty()) {
+                System.out.println("None");
+            } else {
+                System.out.println(result.get(0) + " " + result.get(1));
+            }
+        }
+    }
+
+    private static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
