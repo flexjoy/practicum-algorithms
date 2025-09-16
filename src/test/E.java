d5b9782b3cf6e0837462cb0fae36f0172db68394
Черепанов Сергей (4092324) <slcherepanov@dev.vtb.ru>
2025-09-16 17:31:11 +0300

    Task E. Две фишки - 2

---
+++ src/test/E.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class E {
+
+    // Если ответ существует, верните список из двух элементов
+    // Если нет - то верните пустой список
+    private static List<Integer> twoSum(List<Integer> arr, int targetSum) {
+        var seen = new HashSet<Integer>();
+
+        for (Integer i : arr) {
+            int need = targetSum - i;
+
+            if (seen.contains(need)) {
+                return List.of(need, i);
+            }
+
+            seen.add(i);
+        }
+
+        return List.of();
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = readInt(reader);
+            List<Integer> arr = readList(reader);
+            int targetSum = readInt(reader);
+            List<Integer> result = twoSum(arr, targetSum);
+            if (result.isEmpty()) {
+                System.out.println("None");
+            } else {
+                System.out.println(result.get(0) + " " + result.get(1));
+            }
+        }
+    }
+
+    private static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
