8fcb93dbe652b3b6ff9e202185a305c0857a9560
Черепанов Сергей (4092324) <slcherepanov@dev.vtb.ru>
2025-09-16 10:49:09 +0300

    Task A. A+B

---
+++ src/test/TaskA.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Scanner;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Считали первое число.
+        int a = scanner.nextInt();
+        // Считали второе число.
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
