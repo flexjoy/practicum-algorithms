d14889217aa67cdc9fc61f5ec58c9aa55b87ced9
Черепанов Сергей (4092324) <slcherepanov@dev.vtb.ru>
2025-09-16 11:21:39 +0300

    Task A. A+B

--- src/test/A.java
+++ src/test/A.java
@@ -2,14 +2,17 @@ package test;
 
 import java.util.Scanner;
 
-public class TaskA {
+public class A {
+
+    private static int getSum(int a, int b) {
+        return a + b;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Считали первое число.
         int a = scanner.nextInt();
-        // Считали второе число.
         int b = scanner.nextInt();
-        System.out.println(a + b);
+        System.out.println(getSum(a, b));
+        scanner.close();
     }
 }
